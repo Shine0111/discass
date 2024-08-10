4ce66b00268c4382ee87ca705ede5fba07061081
Shine Ran <ranshine9@gmail.com>
2024-08-10 07:16:11 +0300

    auth actions

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,6 @@
+// A route.ts file is used to talk an outside web service used by the app
+
+// export async function GET() {}
+// export async function POST() {}
+
+export { GET, POST } from "@/auth"; // importing and exporting at the same time
