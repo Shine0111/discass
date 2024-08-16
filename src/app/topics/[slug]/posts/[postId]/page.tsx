32d6149248ad4e9cab8b740528c0f19710dd872e
shine <ranshine9@gmail.com>
2024-08-16 11:03:34 +0300

    create comments

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,3 +1,27 @@
-export default function PostShowPage() {
-  return <div>Post Show Page.</div>;
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import PostShow from "@/components/posts/post-show";
+import paths from "@/paths";
+import Link from "next/link";
+import { fetchCommentByPostId } from "@/db/queries/comments";
+import CommentList from "@/components/comments/comment-list";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
+}
+
+export default function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"<"}Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+      <CommentCreateForm postId={postId} startOpen />
+      <CommentList fetchData={() => fetchCommentByPostId(postId)} />
+    </div>
+  );
 }
