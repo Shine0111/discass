4bfbfff5e6d34dacf3b3cfd52243436bcac5e962
shine <ranshine9@gmail.com>
2024-08-16 19:14:37 +0300

    suspense and skeleton loader

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -4,7 +4,8 @@ import paths from "@/paths";
 import Link from "next/link";
 import { fetchCommentByPostId } from "@/db/queries/comments";
 import CommentList from "@/components/comments/comment-list";
-
+import { Suspense } from "react";
+import PostShowLoading from "@/components/posts/post-show-loading";
 interface PostShowPageProps {
   params: {
     slug: string;
@@ -19,7 +20,9 @@ export default function PostShowPage({ params }: PostShowPageProps) {
       <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
         {"<"}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
       <CommentList postId={postId} />
     </div>
